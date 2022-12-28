38977622d8dfd386b5365a071907a91cfaa8b581
iBiber <iBiber@outlook.de>
2022-12-28 15:01:27 +0100

    Add page title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   publicPath: './',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'GfA Kalender Aufbereitung'
+    }
+  },
   pluginOptions: {
     quasar: {
       importStrategy: 'kebab',
